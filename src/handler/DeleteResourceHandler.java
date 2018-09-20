f460a314d5215328ee34d971646ad94ec49dcf24
z00h230741 <z00h230741@A050931913.rfg.rfgad.ricoh.com>
2018-09-20 15:37:21 +0900

    deleteResourceService

--- src/handler/DeleteResourceHandler.java
+++ src/handler/DeleteResourceHandler.java
@@ -1,7 +1,14 @@
 package handler;
 
 
+import static handler.MessageHolder.*;
+import static handler.ViewHolder.*;
+
+import java.sql.SQLException;
+
 import javax.servlet.http.HttpServletRequest;
+
+import service.DeleteResourceService;
 
 public class DeleteResourceHandler implements Handler {
 	public String handleService(HttpServletRequest request) {
@@ -13,6 +20,33 @@ public class DeleteResourceHandler implements Handler {
 //      //セッションは存在する
 //      authority = (int) httpSession.getAttribute("authority");
 
+        if (authority == 0) {
+
+        	DeleteResourceService deleteResourceService = new DeleteResourceService(request.getParameter("resourceId"));
+
+        	if (deleteResourceService.validate()) {
+
+        		try {
+        			deleteResourceService.execute();
+        			int result = deleteResourceService.getResult();
+
+        			if(result == 1){
+        				request.setAttribute("Pmessage",PM08);
+        				return SHOW_RESOURCE_LIST_SERVLET;
+        			}else{
+        				//ログを残す
+        				return ERROR_PAGE;
+        			}
+
+        		} catch(SQLException e) {
+        			//ログを残す
+        			return ERROR_PAGE;
+        		}
+        	}
+        }else{
+        	//ログを残す
+        	return ERROR_PAGE;
+        }
 		return null;
 	}
 }
