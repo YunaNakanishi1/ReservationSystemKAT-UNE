ba1e3654059ef953e3adf64f9e9a0b90b9985ae6
z00s600114 <z00s600114@ZRFG046920.rfg.rfgad.ricoh.com>
2018-09-20 10:38:24 +0900

    DBHelperとDaoいじってるよ榎

--- src/dao/ResourceDao.java
+++ src/dao/ResourceDao.java
@@ -4,6 +4,12 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
 import dto.Resource;
@@ -13,8 +19,78 @@ import dto.Resource;
  */
 public class ResourceDao {
 
+
+	Connection _con = null;
+
+	/**
+	  * リソース全件の一覧を表示するためのDaoメソッド.
+	  * @return resourceを返す
+	  */
 	public List<Resource> displayAll() {
-		return null;
+		DBHelper dbHelper = new DBHelper();
+
+		_con = dbHelper.connectDb(); //データベースに接続
+
+		String sql = "select resources.resource_id,resource_name,office_name,"
+				+ "category_name,capacity,supplement,usage_stop_start_date,"
+				+ "usage_stop_end_date,deleted "
+				+ "from resources,categories,offices "
+				+ "where resources.category_id = categories.category_id , "
+				+ "resources.office_id = officers.office_id "
+				+ "order by officers.office_id asc and categories.category_id asc "
+				+ "and resources.resource_id asc;";
+
+		Statement stmt = null;
+		ResultSet rs = null;
+
+
+		 if (_con != null) {
+	            try {
+	                stmt = _con.createStatement();
+	                rs = stmt.executeQuery(sql); //実行
+
+	                List<Resource> resourceList = new ArrayList<Resource>();
+
+	                while(rs.next()){
+	                	String resourceId = rs.getString("resource_id");
+	                	String resourceName = rs.getString("resource_name");
+	                	String officeName = rs.getString("office_name");
+	                	String categoryName = rs.getString("category_name");
+	                	int capacity = rs.getInt("capacity");
+	                	String supplement = rs.getString("spplement");
+	                	int deleted = rs.getInt("deleted");
+	                	Timestamp usageStopStartDate = rs.getTimestamp("usage_stop_start_date");
+	                	Timestamp usageStopEndDate = rs.getTimestamp("usage_stop_end_date");
+
+	                	//リストに追加
+	                	resourceList.add(new Resource(resourceId, resourceName, officeName,
+	                			categoryName, capacity, supplement, deleted, null,
+	                			usageStopStartDate, usageStopEndDate));
+
+	            		return resourceList;
+
+	                }
+	            } catch (SQLException e) {
+	                e.printStackTrace();
+	            } finally {
+	                // Statementのクローズ
+	            	if(stmt!= null){
+	            		try {
+		                    dbHelper.closeResource(stmt);
+		                } catch (Exception e) {
+		                    // SQLException以外の例外が発生
+		                    e.printStackTrace();
+		                }
+	            	}
+	                
+
+	                // ヘルパーに接続解除を依頼
+	                dbHelper.closeDb();
+	            }
+	        } else {
+	        	//_conがnullです
+	        	return null;
+	        }
 	}
 
 	public int regist(Resource resource) {
