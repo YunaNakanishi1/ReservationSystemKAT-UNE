879980d30d14ee5d40dc8e89a23ee68396f65463
z00s600124 <z00s600124@ZRFG046930.rfg.rfgad.ricoh.com>
2018-09-20 10:33:46 +0900

    takahara

--- src/handler/Handler.java
+++ src/handler/Handler.java
@@ -1,5 +1,25 @@
+/* Copyright© Ricoh IT Solutions Co.,Ltd.
+ * All Rights Reserved.
+ */
 package handler;
 
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Handlerインターフェースの定義. サーブレットが利用するハンドラーのインターフェース
+ *
+ * @author jinsen
+ *
+ */
 public interface Handler {
+
+    /**
+     * 画面に応じたサービス処理を実行する. 入力チェック、モデルの作成と実行
+     *
+     * @param request 画面から入力された内容を保持しているオブジェクト
+     * @return サービスを実行した結果に応じたViewの名前
+     */
+    //これはpublic
+    String handleService(HttpServletRequest request);
 
 }
--- src/handler/ShowResourceListHandler.java
+++ src/handler/ShowResourceListHandler.java
@@ -1,5 +1,24 @@
 package handler;
 
-public class ShowResourceListHandler {
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import service.ShowResourceListService;
+public class ShowResourceListHandler implements Handler{
+
+    @Override
+    public String handleService(HttpServletRequest request) {
+        ShowResourceListService service = new ShowResourceListService();
+        if(service.validate()){
+            try {
+                service.execute();
+            } catch (SQLException e) {
+                return ;
+            }
+        }else{
+
+        }
+    }
 
 }
