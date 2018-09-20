618b26bf976a1b16f125e487fc0e1cf04d853b8c
z00s600120 <z00s600120@ZRFG046926.rfg.rfgad.ricoh.com>
2018-09-20 14:20:48 +0900

    sessionhandler

--- src/handler/SessionUpdateHandler.java
+++ src/handler/SessionUpdateHandler.java
@@ -1,5 +1,17 @@
 package handler;
 
-public class SessionUpdateHandler {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class SessionUpdateHandler implements Handler{
+
+	@Override
+	public String handleService(HttpServletRequest request) {
+		HttpSession session =request.getSession(false);
+
+
+		return null;
+	}
+
 
 }
