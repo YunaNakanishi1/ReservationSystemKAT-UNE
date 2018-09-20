dac4afced5c5b346b333988cc09e84dcdfd5b265
z00s600109 <z00s600109@ZRFG046915.rfg.rfgad.ricoh.com>
2018-09-20 16:19:15 +0900

    officeDao完成

--- src/dao/OfficeDao.java
+++ src/dao/OfficeDao.java
@@ -4,14 +4,61 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * @author リコーITソリューションズ株式会社 KAT-UNE
  */
 public class OfficeDao {
+	private Connection _con=null;
+	private static Logger _log = LogManager.getLogger();
 
-	public List<String> officeName() {
-		return null;
+	public List<String> officeName() throws SQLException {
+		List<String> officeList=new ArrayList<String>();
+		DBHelper dbHelper =new DBHelper();
+		_con=dbHelper.connectDb();
+
+		String sql="select  office_name from offices order by  office_id";
+
+		Statement stmt=null;
+		ResultSet rs=null;
+
+		if(_con !=null){
+			try{
+				stmt=_con.createStatement();
+				rs=stmt.executeQuery(sql);
+
+				while(rs.next()){
+					 officeList.add(rs.getString(1));
+				}
+			}finally{
+				try{
+					dbHelper.closeResource(stmt);
+
+				}catch(Exception e1){
+					e1.printStackTrace();
+					 _log.error("category() Exception e1");
+				}
+
+				try {
+					dbHelper.closeResource(rs);
+				} catch (Exception e2) {
+					e2.printStackTrace();
+					_log.error("category() Exception e2");
+				}
+				 dbHelper.closeDb();
+			}
+
+		}
+
+		return  officeList;
 	}
 }
