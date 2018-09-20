7e8a2203dfeac85eeb1cb43aaf9783c3ecd3cb80
z00s600120 <z00s600120@ZRFG046926.rfg.rfgad.ricoh.com>
2018-09-20 14:32:37 +0900

    sessionhandler作成

--- src/handler/SessionUpdateHandler.java
+++ src/handler/SessionUpdateHandler.java
@@ -1,7 +1,14 @@
 package handler;
 
+import static handler.MessageHolder.*;
+import static handler.ViewHolder.*;
+
+import java.sql.SQLException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
+import service.CheckAuthorityService;
 
 public class SessionUpdateHandler implements Handler{
 
@@ -9,8 +16,39 @@ public class SessionUpdateHandler implements Handler{
 	public String handleService(HttpServletRequest request) {
 		HttpSession session =request.getSession(false);
 
+		if(session!=null){
+			String userId=(String)session.getAttribute("userId");
 
-		return null;
+			CheckAuthorityService checkAuthorityService =new CheckAuthorityService(userId);
+
+			if(checkAuthorityService.validate()){
+				try{
+					checkAuthorityService.execute();
+
+					int authority = checkAuthorityService.getAuthority();
+
+					if(authority==0||authority==1){
+						session.setAttribute("userId", userId);
+						session.setAttribute("authority", authority);
+						return null;
+					}else{
+						return ERROR_PAGE;
+					}
+				}catch (SQLException e){
+					//ログを残す
+					return ERROR_PAGE;
+				}
+			}else{
+				//ログを残す
+				return ERROR_PAGE;
+			}
+
+
+		}else{
+			request.setAttribute("errorMessage", EM39);
+			return LOG_IN;
+		}
+
 	}
 
 
