2e24cd277d5ab8272dff4836dd976ec3a82a44ed
z00s600109 <z00s600109@ZRFG046915.rfg.rfgad.ricoh.com>
2018-09-20 16:14:58 +0900

    facilityDaoの完成

--- src/dao/FacilityDao.java
+++ src/dao/FacilityDao.java
@@ -4,14 +4,62 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * @author リコーITソリューションズ株式会社 KAT-UNE
  */
 public class FacilityDao {
 
-	public List<String> facility() {
+	private Connection _con=null;
+	private static Logger _log = LogManager.getLogger();
+
+	public List<String> facility() throws SQLException {
+		List<String> facilityList=new ArrayList<String>();
+		DBHelper dbHelper =new DBHelper();
+		_con=dbHelper.connectDb();
+
+		String sql="select resourse_characteristics_name from resourse_characteristics order by resourse_characteristics_id";
+
+		Statement stmt=null;
+		ResultSet rs=null;
+
+		if(_con !=null){
+			try{
+				stmt=_con.createStatement();
+				rs=stmt.executeQuery(sql);
+
+				while(rs.next()){
+					facilityList.add(rs.getString(1));
+				}
+			}finally{
+				try{
+					dbHelper.closeResource(stmt);
+
+				}catch(Exception e1){
+					e1.printStackTrace();
+					 _log.error("category() Exception e1");
+				}
+
+				try {
+					dbHelper.closeResource(rs);
+				} catch (Exception e2) {
+					e2.printStackTrace();
+					_log.error("category() Exception e2");
+				}
+				 dbHelper.closeDb();
+			}
+
+		}
+
 		return null;
 	}
 }
