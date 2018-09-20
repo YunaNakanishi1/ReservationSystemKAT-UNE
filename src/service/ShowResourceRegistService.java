8ae5659390ae9baa439ab7efa6ad51ed4df7c64b
z00s600120 <z00s600120@ZRFG046926.rfg.rfgad.ricoh.com>
2018-09-20 16:27:24 +0900

    showregistservice作成

--- src/service/ShowResourceRegistService.java
+++ src/service/ShowResourceRegistService.java
@@ -1,5 +1,47 @@
 package service;
 
-public class ShowResourceRegistService {
+import java.sql.SQLException;
+import java.util.List;
+
+import dao.CategoryDao;
+import dao.FacilityDao;
+import dao.OfficeDao;
+
+public class ShowResourceRegistService implements Service{
+
+	private List<String> _categoryList;
+	private List<String> _officeList;
+	private List<String> _facilityList;
+
+	@Override
+	public boolean validate() {
+		return true;
+	}
+
+	@Override
+	public void execute() throws SQLException {
+		CategoryDao categoryDao=new CategoryDao();
+		_categoryList=categoryDao.category();
+
+		OfficeDao officeDao=new OfficeDao();
+		_officeList=officeDao.officeName();
+
+		FacilityDao facilityDao = new FacilityDao();
+		_facilityList=facilityDao.facility();
+	}
+
+	public List<String> getCategoryList() {
+		return _categoryList;
+	}
+
+	public List<String> getOfficeList() {
+		return _officeList;
+	}
+
+	public List<String> getFacilityList() {
+		return _facilityList;
+	}
+
+
 
 }
