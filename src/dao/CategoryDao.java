d9740b3006d1a4235adaab32d87b4df455d16dde
z00s600109 <z00s600109@ZRFG046915.rfg.rfgad.ricoh.com>
2018-09-20 16:06:24 +0900

    categoryDao完成

--- src/dao/CategoryDao.java
+++ src/dao/CategoryDao.java
@@ -4,14 +4,63 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * @author リコーITソリューションズ株式会社 KAT-UNE
  */
 public class CategoryDao {
 
-	public List<String> category() {
-		return null;
+	private Connection _con=null;
+	private static Logger _log = LogManager.getLogger();
+
+
+	public List<String> category() throws SQLException {
+		List<String> categoryList=new ArrayList<String>();
+		DBHelper dbHelper =new DBHelper();
+		_con=dbHelper.connectDb();
+
+		String sql="select category_name from categories order by category_id";
+
+		Statement stmt=null;
+		ResultSet rs=null;
+
+		if(_con !=null){
+			try{
+				stmt=_con.createStatement();
+				rs=stmt.executeQuery(sql);
+
+				while(rs.next()){
+					categoryList.add(rs.getString(1));
+				}
+			}finally{
+				try{
+					dbHelper.closeResource(stmt);
+
+				}catch(Exception e1){
+					e1.printStackTrace();
+					 _log.error("category() Exception e1");
+				}
+
+				try {
+					dbHelper.closeResource(rs);
+				} catch (Exception e2) {
+					e2.printStackTrace();
+					_log.error("category() Exception e2");
+				}
+				 dbHelper.closeDb();
+			}
+
+		}
+
+		return categoryList;
 	}
 }
