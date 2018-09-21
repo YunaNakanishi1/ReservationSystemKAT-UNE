b0c3c81cfc32ce354c7fdff1037abe12d41f750a
p000527259 <p000527259@A062844480.rfg.rfgad.ricoh.com>
2018-09-21 10:57:32 +0900

    login修正

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -18,9 +18,8 @@ import dto.User;
  * @author リコーITソリューションズ株式会社 KAT-UNE
  */
 public class UserDao {
-	private static Logger _log = LogManager.getLogger(); //これはクラス図にはないんですが
+	private static Logger _log = LogManager.getLogger();
 	private Connection _con = null;
-
 
 	/**
 	 * ユーザ認証を行うメソッド.
@@ -34,7 +33,6 @@ public class UserDao {
 
 		// ヘルパーに接続を依頼
 		DBHelper dbHelper = new DBHelper();
-
         _con = dbHelper.connectDb();
         PreparedStatement stmt = null;
         ResultSet rs = null;
