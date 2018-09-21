9b241e19f03ba51d71d4498803c1b3c22407bf65
z00s600124 <z00s600124@ZRFG046930.rfg.rfgad.ricoh.com>
2018-09-21 12:01:14 +0900

    takahara

--- src/dao/ResourceDao.java
+++ src/dao/ResourceDao.java
@@ -23,241 +23,245 @@ import dto.Resource;
  */
 public class ResourceDao {
 
-	private static Logger _log = LogManager.getLogger(); //これはクラス図にはないんですが
-	Connection _con = null;
+    private static Logger _log = LogManager.getLogger(); //これはクラス図にはないんですが
+    Connection _con = null;
 
-	/**
-	  * リソース全件の一覧を表示するためのDaoメソッド.
-	  * @return resourceを返す
-	  */
-	public List<Resource> displayAll() throws SQLException{
-		DBHelper dbHelper = new DBHelper();
+    /**
+     * リソース全件の一覧を表示するためのDaoメソッド.
+     * @return resourceを返す
+     */
+    public List<Resource> displayAll() throws SQLException{
+        DBHelper dbHelper = new DBHelper();
+        Statement stmt = null;
+        ResultSet rs = null;
 
-		_con = dbHelper.connectDb(); //データベースに接続
-
-		String sql = "select resources.resource_id,resource_name,office_name,"
-				+ "category_name,capacity,supplement,usage_stop_start_date,"
-				+ "usage_stop_end_date,deleted "
-				+ "from resources,categories,offices "
-				+ "where resources.category_id = categories.category_id and "
-				+ "resources.office_id = offices.office_id "
-				+ "order by offices.office_id asc , categories.category_id asc "
-				+ ", resources.resource_id asc;";
-
-		Statement stmt = null;
-		ResultSet rs = null;
-
-		//空のresourceListを用意
+        //空のresourceListを用意
         List<Resource> resourceList = new ArrayList<Resource>();
 
-		 if (_con != null) {
-	            try {
-	                stmt = _con.createStatement();
-	                rs = stmt.executeQuery(sql); //実行
+        try {
+            _con = dbHelper.connectDb(); //データベースに接続
 
-	                while(rs.next()){
-	                	String resourceId = rs.getString("resource_id");
-	                	String resourceName = rs.getString("resource_name");
-	                	String officeName = rs.getString("office_name");
-	                	String categoryName = rs.getString("category_name");
-	                	int capacity = rs.getInt("capacity");
-	                	String supplement = rs.getString("supplement");
-	                	int deleted = rs.getInt("deleted");
-	                	Timestamp usageStopStartDate = rs.getTimestamp("usage_stop_start_date");
-	                	Timestamp usageStopEndDate = rs.getTimestamp("usage_stop_end_date");
-
-	                	//リストに追加
-	                	resourceList.add(new Resource(resourceId, resourceName, officeName,
-	                			categoryName, capacity, supplement, deleted, null,
-	                			usageStopStartDate, usageStopEndDate));
-
-	                }
-	            } finally {
-	                // Statementのクローズ
-	            	try {
-						dbHelper.closeResource(stmt);
-					} catch (Exception e1) {
-						e1.printStackTrace();
-						 _log.error("displayAll() Exception e1");
-					}
-
-	            	// ResultSetのクローズ
-	            	try {
-						dbHelper.closeResource(rs);
-					} catch (Exception e2) {
-						e2.printStackTrace();
-						_log.error("displayAll() Exception e2");
-					}
-
-	                // ヘルパーに接続解除を依頼
-	                dbHelper.closeDb();
-	            }
-	        }
- 		return resourceList;
-	}
+            String sql = "select resources.resource_id,resource_name,office_name,"
+                    + "category_name,capacity,supplement,usage_stop_start_date,"
+                    + "usage_stop_end_date,deleted "
+                    + "from resources,categories,offices "
+                    + "where resources.category_id = categories.category_id and "
+                    + "resources.office_id = offices.office_id "
+                    + "order by offices.office_id asc , categories.category_id asc "
+                    + ", resources.resource_id asc;";
 
 
-	public int regist(Resource resource) {
-		return 0;
-	}
 
-	public int change(Resource resource) {
-		return 0;
-	}
+            if (_con == null) {
+                _log.error("DatabaseConnectError");
+                throw new SQLException();
+            }
+                stmt = _con.createStatement();
+                rs = stmt.executeQuery(sql); //実行
 
+                while(rs.next()){
+                    String resourceId = rs.getString("resource_id");
+                    String resourceName = rs.getString("resource_name");
+                    String officeName = rs.getString("office_name");
+                    String categoryName = rs.getString("category_name");
+                    int capacity = rs.getInt("capacity");
+                    String supplement = rs.getString("supplement");
+                    int deleted = rs.getInt("deleted");
+                    Timestamp usageStopStartDate = rs.getTimestamp("usage_stop_start_date");
+                    Timestamp usageStopEndDate = rs.getTimestamp("usage_stop_end_date");
 
-	public int delete(String resourceId) throws SQLException {
-		int result = 0;
-		DBHelper dbHelper = new DBHelper();
-		_con = dbHelper.connectDb(); //データベースに接続
-		PreparedStatement stmt = null;
-		String sql = "UPDATE resources SET deleted = 1 WHERE resource_id = ?";
+                    //リストに追加
+                    resourceList.add(new Resource(resourceId, resourceName, officeName,
+                            categoryName, capacity, supplement, deleted, null,
+                            usageStopStartDate, usageStopEndDate));
 
-		if (_con != null) {
-			try {
-				stmt = _con.prepareStatement(sql);
-                stmt.setString(1, resourceId);
-                result = stmt.executeUpdate();
-			} finally {
-				// Statementのクローズ
-            	try {
-					dbHelper.closeResource(stmt);
-				} catch (Exception e1) {
-					e1.printStackTrace();
-					 _log.error("displayAll() Exception e1");
-				}
-			}
+                }
+
+        } finally {
+            // Statementのクローズ
+            try {
+                dbHelper.closeResource(stmt);
+            } catch (Exception e1) {
+                e1.printStackTrace();
+                _log.error("displayAll() Exception e1");
+            }
+
+            // ResultSetのクローズ
+            try {
+                dbHelper.closeResource(rs);
+            } catch (Exception e2) {
+                e2.printStackTrace();
+                _log.error("displayAll() Exception e2");
+            }
 
             // ヘルパーに接続解除を依頼
             dbHelper.closeDb();
-		}
-
-		return result;
-	}
-
-	/**
-	  * リソース詳細を表示するために必要なDao.
-	  * @param resourceId
-	  * @return resourceをリターン
-	 * @throws SQLException
-	  */
-	public Resource displayDetails(String resourceId) throws SQLException {
-		DBHelper dbHelper = new DBHelper();
-
-		_con = dbHelper.connectDb(); //データベースに接続
-
-		PreparedStatement pstmt = null;
-		PreparedStatement pstmt2 = null;
-		PreparedStatement pstmt3 = null;
-		ResultSet rs = null;
-		ResultSet rs2 = null;
-		ResultSet rs3 = null;
-
-		final String sql = "select resource_name,office_name,"
-				+ "capacity,usage_stop_start_date,usage_stop_end_date,supplement "
-				+ "from resources,offices "
-				+ "where offices.office_id = resources.office_id "
-				+ "and resource_id = ?;";
-
-		final String sql2 = "select resource_characteristic_name "
-				+ "from resource_features,resource_characteristics "
-				+ "where resource_id=? and "
-				+ "resource_features.resource_characteristic_id "
-				+ "= resource_characteristics.resource_characteristic_id "
-				+ "order by resource_features.resource_characteristic_id;";
-
-		final String sql3 = "select category_name "
-				+ "from resources,categories "
-				+ "where resource_id=? and categories.category_id=resources.category_id "
-				+ "order by categories.category_id;";
-
-		if(_con != null){
-
-			String resourceName = "";
-			String officeName = "";
-			int capacity = 0;
-			String category = "";
-			String supplement = "";
-			Timestamp usageStopStartDate = null;
-			Timestamp usageStopEndDate = null;
-
-			List<String> facilityList = new ArrayList<String>();
+        }
+        return resourceList;
+    }
 
 
-			try{
+    public int regist(Resource resource) {
+        return 0;
+    }
 
-				//選んだリソースの詳細を表示するためにIDをもとにデータベースからresourceDTO
-				//をとってくる
-				pstmt = _con.prepareStatement(sql);
-				pstmt.setString(1, resourceId);
-
-				rs = pstmt.executeQuery(); //実行
-
-				while(rs.next()){
-					resourceName = rs.getString("resource_name");
-					officeName = rs.getString("office_name");
-					capacity = rs.getInt("capacity");
-					supplement = rs.getString("supplement");
-					usageStopStartDate = rs.getTimestamp("usage_stop_start_date");
-					usageStopEndDate = rs.getTimestamp("usage_stop_end_date");
-				}
-
-				//設備表示のために設備のリストを作成
-				pstmt2 = _con.prepareStatement(sql2);
-				pstmt2.setString(1, resourceId);
-
-				rs2 = pstmt2.executeQuery(); //実行
-
-				while(rs2.next()){
-					String facility = rs2.getString("resource_characteristic_name");
-					facilityList.add(facility);
-				}
-
-				//カテゴリ表示のためにカテゴリのリストを作成
-				//設備表示のために設備のリストを作成
-				pstmt3 = _con.prepareStatement(sql3);
-				pstmt3.setString(1, resourceId);
-
-				rs3 = pstmt3.executeQuery(); //実行
-
-				while(rs3.next()){
-					category = rs3.getString("category_name");
-				}
+    public int change(Resource resource) {
+        return 0;
+    }
 
 
-				Resource resource = new Resource(resourceId, resourceName, officeName,
-						category, capacity, supplement, 0, facilityList, usageStopStartDate, usageStopEndDate);
+    public int delete(String resourceId) throws SQLException {
+        int result = 0;
+        DBHelper dbHelper = new DBHelper();
+        _con = dbHelper.connectDb(); //データベースに接続
+        PreparedStatement stmt = null;
+        String sql = "UPDATE resources SET deleted = 1 WHERE resource_id = ?";
 
-				return resource;
+        if (_con != null) {
+            try {
+                stmt = _con.prepareStatement(sql);
+                stmt.setString(1, resourceId);
+                result = stmt.executeUpdate();
+            } finally {
+                // Statementのクローズ
+                try {
+                    dbHelper.closeResource(stmt);
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                    _log.error("displayAll() Exception e1");
+                }
+            }
+
+            // ヘルパーに接続解除を依頼
+            dbHelper.closeDb();
+        }
+
+        return result;
+    }
+
+    /**
+     * リソース詳細を表示するために必要なDao.
+     * @param resourceId
+     * @return resourceをリターン
+     * @throws SQLException
+     */
+    public Resource displayDetails(String resourceId) throws SQLException {
+        DBHelper dbHelper = new DBHelper();
+
+        _con = dbHelper.connectDb(); //データベースに接続
+
+        PreparedStatement pstmt = null;
+        PreparedStatement pstmt2 = null;
+        PreparedStatement pstmt3 = null;
+        ResultSet rs = null;
+        ResultSet rs2 = null;
+        ResultSet rs3 = null;
+
+        final String sql = "select resource_name,office_name,"
+                + "capacity,usage_stop_start_date,usage_stop_end_date,supplement "
+                + "from resources,offices "
+                + "where offices.office_id = resources.office_id "
+                + "and resource_id = ?;";
+
+        final String sql2 = "select resource_characteristic_name "
+                + "from resource_features,resource_characteristics "
+                + "where resource_id=? and "
+                + "resource_features.resource_characteristic_id "
+                + "= resource_characteristics.resource_characteristic_id "
+                + "order by resource_features.resource_characteristic_id;";
+
+        final String sql3 = "select category_name "
+                + "from resources,categories "
+                + "where resource_id=? and categories.category_id=resources.category_id "
+                + "order by categories.category_id;";
+
+        if(_con != null){
+
+            String resourceName = "";
+            String officeName = "";
+            int capacity = 0;
+            String category = "";
+            String supplement = "";
+            Timestamp usageStopStartDate = null;
+            Timestamp usageStopEndDate = null;
+
+            List<String> facilityList = new ArrayList<String>();
 
 
-			}finally{
-				// PreparedStatementのクローズ
-            	try {
-					dbHelper.closeResource(pstmt);
-				} catch (Exception e3) {
-					e3.printStackTrace();
-					 _log.error("displayAll() Exception e3");
-				}
+            try{
 
-            	// ResultSetのクローズ
-            	try {
-					dbHelper.closeResource(rs);
-				} catch (Exception e4) {
-					e4.printStackTrace();
-					_log.error("displayAll() Exception e4");
-				}
+                //選んだリソースの詳細を表示するためにIDをもとにデータベースからresourceDTO
+                //をとってくる
+                pstmt = _con.prepareStatement(sql);
+                pstmt.setString(1, resourceId);
+
+                rs = pstmt.executeQuery(); //実行
+
+                while(rs.next()){
+                    resourceName = rs.getString("resource_name");
+                    officeName = rs.getString("office_name");
+                    capacity = rs.getInt("capacity");
+                    supplement = rs.getString("supplement");
+                    usageStopStartDate = rs.getTimestamp("usage_stop_start_date");
+                    usageStopEndDate = rs.getTimestamp("usage_stop_end_date");
+                }
+
+                //設備表示のために設備のリストを作成
+                pstmt2 = _con.prepareStatement(sql2);
+                pstmt2.setString(1, resourceId);
+
+                rs2 = pstmt2.executeQuery(); //実行
+
+                while(rs2.next()){
+                    String facility = rs2.getString("resource_characteristic_name");
+                    facilityList.add(facility);
+                }
+
+                //カテゴリ表示のためにカテゴリのリストを作成
+                //設備表示のために設備のリストを作成
+                pstmt3 = _con.prepareStatement(sql3);
+                pstmt3.setString(1, resourceId);
+
+                rs3 = pstmt3.executeQuery(); //実行
+
+                while(rs3.next()){
+                    category = rs3.getString("category_name");
+                }
+
+
+                Resource resource = new Resource(resourceId, resourceName, officeName,
+                        category, capacity, supplement, 0, facilityList, usageStopStartDate, usageStopEndDate);
+
+                return resource;
+
+
+            }finally{
+                // PreparedStatementのクローズ
+                try {
+                    dbHelper.closeResource(pstmt);
+                } catch (Exception e3) {
+                    e3.printStackTrace();
+                    _log.error("displayAll() Exception e3");
+                }
+
+                // ResultSetのクローズ
+                try {
+                    dbHelper.closeResource(rs);
+                } catch (Exception e4) {
+                    e4.printStackTrace();
+                    _log.error("displayAll() Exception e4");
+                }
 
                 // ヘルパーに接続解除を依頼
                 dbHelper.closeDb();
-			}
+            }
 
-		}
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	public String getMaxId() {
-		return null;
-	}
+    public String getMaxId() {
+        return null;
+    }
 }
--- src/handler/ShowResourceListHandler.java
+++ src/handler/ShowResourceListHandler.java
@@ -16,7 +16,7 @@ import dto.Resource;
 import service.ShowResourceListService;
 public class ShowResourceListHandler implements Handler{
 
-    private static Logger _log = LogManager.getLogger(); //これはクラス図にはないんですが
+    private static Logger _log = LogManager.getLogger();
 
     @Override
     public String handleService(HttpServletRequest request) {
@@ -29,6 +29,8 @@ public class ShowResourceListHandler implements Handler{
 
 
         ShowResourceListService service = new ShowResourceListService();
+        List<Resource> resourceList;
+
         if(service.validate()){
             try {
                 //実行
@@ -38,58 +40,60 @@ public class ShowResourceListHandler implements Handler{
                 _log.error("SQLException");
                 return ViewHolder.ERROR_PAGE;
             }
-                List<Resource> resourceList = service.getResourceList();
-
-                if(authority != 0){
-                    //権限のないユーザの処理
-                    List<Resource> forNotAuthorityResourceList = new ArrayList<>();
-                    //削除済みでないものをリストにセット
-                    for (Resource resource : resourceList) {
-                        if(resource.getDeleted() == 0){
-                            forNotAuthorityResourceList.add(resource);
-                        }
-                    }
-                    //権限のない人用のリストをセットするためのリストに代入
-                    resourceList = forNotAuthorityResourceList;//追記
-
-//                    if(forNotAuthorityResourceList.size() == 0){
-//                        request.setAttribute("Emessage", "検索結果は0件です。");
-//                    }
-//                    request.setAttribute("resourceList", forNotAuthorityResourceList);
-//                    request.setAttribute("resourceListSize", forNotAuthorityResourceList.size());
-                }
-                //else{
-                    //権限のあるユーザとないユーザの同時処理
-                    if(resourceList.size() == 0){
-                        request.setAttribute("Emessage", "検索結果は0件です。");
-                    }
-                    request.setAttribute("resourceList", resourceList);
-                    request.setAttribute("resourceListSize", resourceList.size());
-
-                    //ステータスの設定
-                    List<String> statusList = new ArrayList<>();
-                    //現在時刻取得
-                    Date now = Calendar.getInstance().getTime();
-
-                    for (Resource resource : resourceList) {
-                        if(resource.getDeleted() == 1){
-                            statusList.add("削除済み");
-                        }
-                        else if(resource.getUsageStopStartDate().after(now) && resource.getUsageStopEndDate().before(now)){
-                            statusList.add("利用停止");
-                        }else{
-                            statusList.add("利用可能");
-                        }
-                    }
-                    request.setAttribute("statusList", statusList);
-
-                //}
-                return ViewHolder.RESOURCE_LIST;
+            resourceList = service.getResourceList();
 
         }else{
             _log.error("validateError");
             return ViewHolder.ERROR_PAGE;
         }
+
+        if(authority != 0){
+            //権限のないユーザの処理
+            List<Resource> forNotAuthorityResourceList = new ArrayList<>();
+            //削除済みでないものをリストにセット
+            for (Resource resource : resourceList) {
+                if(resource.getDeleted() == 0){
+                    forNotAuthorityResourceList.add(resource);
+                }
+            }
+            //権限のない人用のリストをセットするためのリストに代入
+            resourceList = forNotAuthorityResourceList;//追記
+
+            //                    if(forNotAuthorityResourceList.size() == 0){
+            //                        request.setAttribute("Emessage", "検索結果は0件です。");
+            //                    }
+            //                    request.setAttribute("resourceList", forNotAuthorityResourceList);
+            //                    request.setAttribute("resourceListSize", forNotAuthorityResourceList.size());
+        }
+        //else{
+        //権限のあるユーザとないユーザの同時処理
+        if(resourceList.size() == 0){
+            request.setAttribute("Emessage", "検索結果は0件です。");
+        }
+        request.setAttribute("resourceList", resourceList);
+        request.setAttribute("resourceListSize", resourceList.size());
+
+        //ステータスの設定
+        List<String> statusList = new ArrayList<>();
+        //現在時刻取得
+        Date now = Calendar.getInstance().getTime();
+
+        for (Resource resource : resourceList) {
+            if(resource.getDeleted() == 1){
+                statusList.add("削除済み");
+            }
+            else if(resource.getUsageStopStartDate().after(now) && resource.getUsageStopEndDate().before(now)){
+                statusList.add("利用停止");
+            }else{
+                statusList.add("利用可能");
+            }
+        }
+        request.setAttribute("statusList", statusList);
+
+        //}
+        return ViewHolder.RESOURCE_LIST;
+
+
 
     }
 
