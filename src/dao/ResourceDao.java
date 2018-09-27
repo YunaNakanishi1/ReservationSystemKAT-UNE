6ef3fa244677addbb3c287f0273ae85d2bb9c21c
p000527259 <p000527259@A062844480.rfg.rfgad.ricoh.com>
2018-09-27 15:59:01 +0900

    delコメント追加

--- src/dao/ResourceDao.java
+++ src/dao/ResourceDao.java
@@ -121,8 +121,12 @@ public class ResourceDao {
         PreparedStatement stmt = null;
         String sql = "UPDATE resources SET deleted = 1 WHERE resource_id = ?";
 
-		if (_con != null) {
+
         try {
+            if (_con == null) {
+                _log.error("DatabaseConnectError");
+                throw new SQLException();
+            }
             stmt = _con.prepareStatement(sql);
             stmt.setString(1, resourceId);
             result = stmt.executeUpdate();
@@ -134,7 +138,6 @@ public class ResourceDao {
                 e1.printStackTrace();
                 _log.error("displayAll() Exception e1");
             }
-		}
 
             // ヘルパーに接続解除を依頼
         dbHelper.closeDb();
