53c86949dc5d2c805ab19db731d1e3c51312d38c
p000527259 <p000527259@A062844480.rfg.rfgad.ricoh.com>
2018-09-28 12:12:30 +0900

    testpush

--- test/service/DeleteResourceServiceTest.java
+++ test/service/DeleteResourceServiceTest.java
@@ -1,11 +1,65 @@
 package service;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+
 public class DeleteResourceServiceTest {
 
-	//@Test
-//	public void test1() {
-//		DeleteResourceService drs = new DeleteResourceService("r006");
-//		assertThat(drs.get)
-//	}
+	@Test
+	public void test1() {
+		DeleteResourceService drs = new DeleteResourceService("r006");
+		assertThat(drs.getResourceId(), is("r006"));
+	}
 
+	@Test
+	public void test2() {
+		DeleteResourceService drs = new DeleteResourceService("r001");
+		try {
+			drs.execute();
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		assertThat(drs.getResult(), is(1));
+	}
+
+	@Test
+	public void test3() {
+		DeleteResourceService drs = new DeleteResourceService("u005");
+		try {
+			drs.execute();
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		assertThat(drs.getResult(), is(0));
+	}
+
+	@Test
+	public void test4() {
+		DeleteResourceService drs = new DeleteResourceService(null);
+		try {
+			drs.execute();
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		assertThat(drs.getResult(), is(0));
+	}
+
+	@Test
+	public void test5() {
+		DeleteResourceService drs = new DeleteResourceService("r006");
+		try {
+			drs.execute();
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		assertThat(drs.getResult(), is(0));
+	}
 }
