097604232c1fc877efcd122d81fc142d4871016d
z00s600109 <z00s600109@ZRFG046915.rfg.rfgad.ricoh.com>
2018-09-28 14:29:01 +0900

    ServiceValidator 作成途中

--- src/service/ServiceValidator.java
+++ src/service/ServiceValidator.java
@@ -1,5 +1,42 @@
 package service;
 
+import static handler.MessageHolder.*;
+
+import dto.Resource;
+
 public class ServiceValidator {
+
+
+	private String _validateMessage;
+
+
+	public String getValidateMessage() {
+		return _validateMessage;
+	}
+
+
+	public boolean setResourseDetailValidate(Resource resource){
+
+
+		String resourceName=resource.getResourceName();
+		if(resourceName.length()>30){
+			_validateMessage=EM28;
+			return false;
+
+		}
+
+		int capacity=resource.getCapacity();
+		if(capacity<0||999<capacity){
+			_validateMessage=EM32;
+			return false;
+
+		}
+
+
+
+
+	}
+
+
 
 }
