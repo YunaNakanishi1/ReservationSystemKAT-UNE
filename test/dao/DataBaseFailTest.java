f23a6f33c0ffcfd0d91f395db2a41c30297c6486
p000527259 <p000527259@A062844480.rfg.rfgad.ricoh.com>
2018-09-28 17:13:17 +0900

    データベース切断テスト

--- test/dao/DataBaseFailTest.java
+++ test/dao/DataBaseFailTest.java
@@ -14,40 +14,26 @@ public class DataBaseFailTest {
 
 	//delete(String resourceId)データベース切断
 	@Test(expected = SQLException.class)
-	public void deletetest2_5() {
+	public void deletetest2_5() throws SQLException  {
 		ResourceDao rd = new ResourceDao();
-		try {
-			assertThat(rd.delete("u002"), is(0));
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		assertThat(rd.delete("u002"), is(0));
+
 
 	}
 
 	//getUser(user)データベース切断
 	@Test(expected = SQLException.class)
-	public void getUsertest1_13() {
+	public void getUsertest1_13() throws SQLException {
 		UserDao ud = new UserDao();
 		User user = new User("u0123456", "password", 0);
-		try {
-			assertThat(ud.getUser(user), nullValue());
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		assertThat(ud.getUser(user), nullValue());
 	}
 
 	//getUser(user)データベース切断
 	@Test(expected = SQLException.class)
-	public void getAuthoritytest2_5() {
+	public void getAuthoritytest2_5()  throws SQLException  {
 		UserDao ud = new UserDao();
-		try {
-			ud.getAuthority("u0123456");
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		ud.getAuthority("u0123456");
 	}
 
 	/**
