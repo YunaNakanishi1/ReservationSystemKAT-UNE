4fc6a36c13426ed193074ace0530b0de4f4369c0
z00s600114 <z00s600114@ZRFG046920.rfg.rfgad.ricoh.com>
2018-09-28 14:33:12 +0900

    変になってないか確認して

--- test/dao/ResourceDaoTest_dataSet1_use.java
+++ test/dao/ResourceDaoTest_dataSet1_use.java
@@ -49,59 +49,59 @@ public class ResourceDaoTest_dataSet1_use {
 	}
 
 
-	//@Test
-	public void test2_1() {
-		ResourceDao rd = new ResourceDao();
-		try {
-			assertThat(rd.delete("r001"), is(1));
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
-	}
+//	//@Test
+//	public void test2_1() {
+//		ResourceDao rd = new ResourceDao();
+//		try {
+//			assertThat(rd.delete("r001"), is(1));
+//		} catch (SQLException e) {
+//			// TODO 自動生成された catch ブロック
+//			e.printStackTrace();
+//		}
+//	}
+//
+//	//@Test
+//	public void test2_2() {
+//		ResourceDao rd = new ResourceDao();
+//		try {
+//			assertThat(rd.delete("u002"), is(0));
+//		} catch (SQLException e) {
+//			// TODO 自動生成された catch ブロック
+//			e.printStackTrace();
+//		}
+//	}
+//
+//	//@Test
+//	public void test2_3() {
+//		ResourceDao rd = new ResourceDao();
+//		try {
+//			assertThat(rd.delete(null), is(0));
+//		} catch (SQLException e) {
+//			// TODO 自動生成された catch ブロック
+//			e.printStackTrace();
+//		}
+//	}
+//
+//	//@Test
+//	public void test2_4() {
+//		ResourceDao rd = new ResourceDao();
+//		try {
+//			assertThat(rd.delete("A\'or\'A\'=\'A\'"), is(0));
+//		} catch (SQLException e) {
+//			// TODO 自動生成された catch ブロック
+//			e.printStackTrace();
+//		}
+//	}
+//
+//	@Test
+//	public void test2_5() {
+//		ResourceDao rd = new ResourceDao();
+//		try {
+//			assertThat(rd.delete("u002"), is(0));
+//		} catch (SQLException e) {
+//			// TODO 自動生成された catch ブロック
+//			e.printStackTrace();
+//		}
+//	}
 
-	//@Test
-	public void test2_2() {
-		ResourceDao rd = new ResourceDao();
-		try {
-			assertThat(rd.delete("u002"), is(0));
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
-	}
-
-	//@Test
-	public void test2_3() {
-		ResourceDao rd = new ResourceDao();
-		try {
-			assertThat(rd.delete(null), is(0));
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
-	}
-
-	//@Test
-	public void test2_4() {
-		ResourceDao rd = new ResourceDao();
-		try {
-			assertThat(rd.delete("A\'or\'A\'=\'A\'"), is(0));
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
-	}
-
-	@Test
-	public void test2_5() {
-		ResourceDao rd = new ResourceDao();
-		try {
-			assertThat(rd.delete("u002"), is(0));
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
-	}
-
-}
+//}
