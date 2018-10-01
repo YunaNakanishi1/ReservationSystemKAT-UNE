2f257c52ff264551bc3bd7298da8f8aeae8c82d5
p000527259 <p000527259@A062844480.rfg.rfgad.ricoh.com>
2018-10-01 15:23:55 +0900

    userdaotest

--- test/dao/UserDaoTest.java
+++ test/dao/UserDaoTest.java
@@ -11,194 +11,127 @@ import dto.User;
 
 public class UserDaoTest {
 
-//	@Test
-	public void test1_1() {
+	@Test
+	public void test1_1() throws SQLException {
 		UserDao ud = new UserDao();
 		User user = new User("u0123456", "password", 0);
-		try {
-			assertThat(ud.getUser(user).getUserId(), is("u0123456"));
-			assertThat(ud.getUser(user).getPassword(), is("password"));
-			assertThat(ud.getUser(user).getAuthority(), is(1));
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		assertThat(ud.getUser(user).getUserId(), is("u0123456"));
+		assertThat(ud.getUser(user).getPassword(), is("password"));
+		assertThat(ud.getUser(user).getAuthority(), is(1));
+
 	}
 
-//	@Test
-	public void test1_2() {
+	@Test
+	public void test1_2() throws SQLException {
 		UserDao ud = new UserDao();
 		User user = new User("u1123456", "password", 0);
-		try {
-			assertThat(ud.getUser(user), nullValue());
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		assertThat(ud.getUser(user), nullValue());
+
 	}
 
-	//@Test
-	public void test1_3() {
+	@Test
+	public void test1_3() throws SQLException {
 		UserDao ud = new UserDao();
 		User user = new User("u0123456", "pass", 0);
-		try {
-			assertThat(ud.getUser(user), nullValue());
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		assertThat(ud.getUser(user), nullValue());
+
 	}
 
-	//@Test
-	public void test1_4() {
+	@Test
+	public void test1_4() throws SQLException {
 		UserDao ud = new UserDao();
 		User user = new User("u1123456", "pass", 0);
-		try {
-			assertThat(ud.getUser(user), nullValue());
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		assertThat(ud.getUser(user), nullValue());
+
 	}
 
-	//@Test
-	public void test1_5() {
+	@Test
+	public void test1_5() throws SQLException {
 		UserDao ud = new UserDao();
 		User user = new User("u1123456", null, 0);
-		try {
-			assertThat(ud.getUser(user), nullValue());
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		assertThat(ud.getUser(user), nullValue());
+
 	}
 
-	//@Test
-	public void test1_6() {
+	@Test
+	public void test1_6() throws SQLException {
 		UserDao ud = new UserDao();
 		User user = new User(null, "pass", 0);
-		try {
-			assertThat(ud.getUser(user), nullValue());
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		assertThat(ud.getUser(user), nullValue());
+
 	}
 
-	//@Test
-	public void test1_7() {
+	@Test
+	public void test1_7() throws SQLException {
 		UserDao ud = new UserDao();
 		User user = new User("u0123456", null, 0);
-		try {
-			assertThat(ud.getUser(user), nullValue());
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		assertThat(ud.getUser(user), nullValue());
+
 	}
 
-	//@Test
-	public void test1_8() {
+	@Test
+	public void test1_8() throws SQLException {
 		UserDao ud = new UserDao();
 		User user = new User(null, "password", 0);
-		try {
-			assertThat(ud.getUser(user), nullValue());
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		assertThat(ud.getUser(user), nullValue());
 	}
 
-	//@Test
-	public void test1_9() {
+	@Test
+	public void test1_9() throws SQLException {
 		UserDao ud = new UserDao();
 		User user = new User(null, null, 0);
-		try {
-			assertThat(ud.getUser(user), nullValue());
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		assertThat(ud.getUser(user), nullValue());
 	}
 
-	//@Test
-	public void test1_10() {
+	@Test
+	public void test1_10() throws SQLException {
 		UserDao ud = new UserDao();
 		User user = null;
-		try {
 			assertThat(ud.getUser(user), nullValue());
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+
 	}
 
 	//@Test
-	public void test1_11() {
+	public void test1_11() throws SQLException {
 		UserDao ud = new UserDao();
 		User user = new User("A\'or\'A\'=\'A\'", "A\'or\'A\'=\'A\'", 0);
-		try {
-			assertThat(ud.getUser(user), nullValue());
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		assertThat(ud.getUser(user), nullValue());
+
 	}
 
 	@Test
-	public void test1_12() {
+	public void test1_12() throws SQLException {
 		UserDao ud = new UserDao();
 		User user = new User("u0000007", "password", 0);
-		try {
-			assertThat(ud.getUser(user), nullValue());
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		assertThat(ud.getUser(user), nullValue());
+
 	}
 
 	@Test
-	public void test2_1() {
+	public void test2_1() throws SQLException {
 		UserDao ud = new UserDao();
-		try {
-			assertThat(ud.getAuthority("u0123456"), is(1));
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		assertThat(ud.getAuthority("u0123456"), is(1));
+
 	}
 
 	@Test
-	public void test2_2() {
+	public void test2_2() throws SQLException {
 		UserDao ud = new UserDao();
-		try {
-			assertThat(ud.getAuthority(null), is(-1));
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		assertThat(ud.getAuthority(null), is(-1));
+
 	}
 
 	@Test
-	public void test2_3() {
+	public void test2_3() throws SQLException {
 		UserDao ud = new UserDao();
-		try {
-			assertThat(ud.getAuthority("nothing"), is(-1));
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		assertThat(ud.getAuthority("nothing"), is(-1));
+
 	}
 
 	@Test
-	public void test2_4() {
+	public void test2_4() throws SQLException {
 		UserDao ud = new UserDao();
-		try {
-			assertThat(ud.getAuthority("A\'or\'A\'=\'A\'"), is(-1));
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		assertThat(ud.getAuthority("A\'or\'A\'=\'A\'"), is(-1));
 	}
 
 
