9aef5d2c71f5bbb3694ef2b691178fdb96883894
z00s600120 <z00s600120@ZRFG046926.rfg.rfgad.ricoh.com>
2018-10-02 11:52:30 +0900

    registResourceService作成途中

--- src/service/RegistResourceService.java
+++ src/service/RegistResourceService.java
@@ -7,7 +7,7 @@ import java.util.List;
 import dao.ResourceDao;
 import dto.Resource;
 
-public class RegistResourceService implements Service{
+public class RegistResourceService implements Service {
 
 	private String _validationMessage;
 	private Resource _resource;
@@ -19,75 +19,74 @@ public class RegistResourceService implements Service{
 		_resource = resource;
 	}
 
-
 	@Override
 	public boolean validate() {
 		ServiceValidator serviceValidator = new ServiceValidator();
 		boolean validate = serviceValidator.setResourseDetailValidate(_resource);
-		if(!validate){
-			_validationMessage=serviceValidator.getValidationMessage();
+		if (!validate) {
+			_validationMessage = serviceValidator.getValidationMessage();
 		}
 		return validate;
 	}
 
 	@Override
 	public void execute() throws SQLException {
-		//全リソースIDを取得
-		ResourceDao resourceDao=new ResourceDao();
-		List<String> resourceIdList=resourceDao.getMaxId();
-		//最大のIDを求めるための変数を用意
+		// 全リソースIDを取得
+		ResourceDao resourceDao = new ResourceDao();
+		List<String> resourceIdList = resourceDao.getMaxId();
+		// 最大のIDを求めるための変数を用意
 		BigInteger maxIdInteger = new BigInteger("0");
-		//IDの数字部分の最大値を求める
-		for(String resourceId:resourceIdList){
-			//数字以外の部分を取り除く
-			String resourceIdNumber=resourceId.replaceAll("[^0-9]", "");
+		// IDの数字部分の最大値を求める
+		for (String resourceId : resourceIdList) {
+			// 数字以外の部分を取り除く
+			String resourceIdNumber = resourceId.replaceAll("[^0-9]", "");
 			try {
-				//数字部分を整数に直す
+				// 数字部分を整数に直す
 				BigInteger resourceIdInteger = new BigInteger(resourceIdNumber);
-				//これまでの最大値より大きければ置き換える
-				if(maxIdInteger.compareTo(resourceIdInteger)<0){
-					maxIdInteger=resourceIdInteger;
+				// これまでの最大値より大きければ置き換える
+				if (maxIdInteger.compareTo(resourceIdInteger) < 0) {
+					maxIdInteger = resourceIdInteger;
 				}
 			} catch (NumberFormatException e) {
-				//数字に直せないIDは無視
+				// 数字に直せないIDは無視
 			}
 		}
-		//IDの数字の最大値に1足す
-		maxIdInteger=maxIdInteger.add(new BigInteger("1"));
-		//文字列に直す
+		// IDの数字の最大値に1足す
+		maxIdInteger = maxIdInteger.add(new BigInteger("1"));
+		// 文字列に直す
 		String MaxIdNumber = maxIdInteger.toString();
-		//最大桁に対して桁数が残っているか調べる
-		int leftLength=19-MaxIdNumber.length();
-		if(leftLength<=0){
-			_resourceId=null;
+		// 最大桁に対して桁数が残っているか調べる
+		int leftLength = 19 - MaxIdNumber.length();
+		if (leftLength <= 0) {
+			_resourceId = null;
 			return;
 		}
-		//IDの補完する部分を求める
-		String formerOfResourceId="";
-		for(int i=0;i<leftLength;i++){
-			formerOfResourceId="0"+formerOfResourceId;
+		// IDの補完する部分を求める
+		String formerOfResourceId = "";
+		for (int i = 0; i < leftLength; i++) {
+			formerOfResourceId = "0" + formerOfResourceId;
 		}
-		formerOfResourceId="r"+formerOfResourceId;
-		//前半部分と数字部分を結合する
-		_resourceId=formerOfResourceId+MaxIdNumber;
+		formerOfResourceId = "r" + formerOfResourceId;
+		// 前半部分と数字部分を結合する
+		_resourceId = formerOfResourceId + MaxIdNumber;
+
+		_resource = new Resource(_resourceId, _resource.getResourceName(), _resource.getOfficeName(),
+				_resource.getCategory(), _resource.getCapacity(), _resource.getSupplement(), 0, _resource.getFacility(),
+				_resource.getUsageStopStartDate(), _resource.getUsageStopEndDate());
+		_result=resourceDao.regist(_resource);
 
 	}
-
 
 	public String getValidationMessage() {
 		return _validationMessage;
 	}
 
-
 	public int getResult() {
 		return _result;
 	}
 
-
 	public String getResourceId() {
 		return _resourceId;
 	}
-
-
 
 }
