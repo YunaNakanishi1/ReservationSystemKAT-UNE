4308f3413fc4656bd3ee748982ee16da8bcddfe3
z00s600120 <z00s600120@ZRFG046926.rfg.rfgad.ricoh.com>
2018-10-03 09:59:40 +0900

    登録変更機能修正

--- src/handler/SetResourceDetailsHandler.java
+++ src/handler/SetResourceDetailsHandler.java
@@ -20,7 +20,9 @@ import dto.Resource;
 import service.ChangeResourceService;
 import service.RegistResourceService;
 
-/**リソースの登録、変更を行う.
+/**
+ * リソースの登録、変更を行う.
+ *
  * @author リコーITソリューションズ株式会社 KAT-UNE
  *
  */
@@ -38,39 +40,41 @@ public class SetResourceDetailsHandler implements Handler {
 		HttpSession session = request.getSession(false);
 		int authority = (int) session.getAttribute("authority");
 
-		if(authority==0){
-			//表示する内容があることを示す
+		if (authority == 0) {
+			// 表示する内容があることを示す
 			request.setAttribute("hasResourceData", true);
 
-			//機能に依存しないバリデーションチェック
-			if(precheck()){
-				//typeの値に応じて登録、変更を行う
-				if("regist".equals(_type)){
+			// 機能に依存しないバリデーションチェック
+			if (precheck()) {
+				// typeの値に応じて登録、変更を行う
+				if ("regist".equals(_type)) {
 					return regist();
-				}else{
+				} else {
 					return change();
 				}
-			}else{
-				//入力に不備があればリソース入力ページに戻す
+			} else {
+				// 入力に不備があればリソース入力ページに戻す
 				return RESOURCE_REGIST_SERVLET;
 			}
 
-		}else{
+		} else {
 			_log.error("no authority");
 			return ERROR_PAGE;
 		}
 
 	}
 
-	/**入力内容の取得と機能に依存しないバリデーションチェック.
+	/**
+	 * 入力内容の取得と機能に依存しないバリデーションチェック.
+	 *
 	 * @return チェック結果
 	 */
 	private boolean precheck() {
-		//登録か変更かを取得し、フィールドとリクエストにセットする
+		// 登録か変更かを取得し、フィールドとリクエストにセットする
 		_type = _request.getParameter("type");
 		_request.setAttribute("type", _type);
 
-		//入力内容を取得し、再表示用にsetAttributeしなおす
+		// 入力内容を取得し、再表示用にsetAttributeしなおす
 		String resourceId = _request.getParameter("resourceId");
 		_request.setAttribute("resourceId", resourceId);
 		String resourceName = _request.getParameter("resourceName");
@@ -96,100 +100,104 @@ public class SetResourceDetailsHandler implements Handler {
 		String supplement = _request.getParameter("supplement");
 		_request.setAttribute("supplement", supplement);
 
-		//リソース特性をリストとしてセット
+		// リソース特性をリストとしてセット
 		List<String> facility = new ArrayList<String>();
-		String[] facilityArray=_request.getParameterValues("facility");
-		if(facilityArray!=null){
-		facility = new ArrayList<String>(Arrays.asList(facilityArray));
+		String[] facilityArray = _request.getParameterValues("facility");
+		if (facilityArray != null) {
+			facility = new ArrayList<String>(Arrays.asList(facilityArray));
 		}
 		_request.setAttribute("facility", facility);
 
 		CommonValidator commonValidator = new CommonValidator();
 
-		//リソース名が入力されているか調べる
+		// リソース名が入力されているか調べる
 		if (commonValidator.notSetOn(resourceName)) {
 			_request.setAttribute("Emessage", EM27);
 			return false;
 		}
-		//カテゴリ情報があるか調べる
+		// カテゴリ情報があるか調べる
 		if (commonValidator.notSetOn(category)) {
 			_request.setAttribute("Emessage", EM37);
 			return false;
 		}
-		//定員が入力されているか調べる
+		// 定員が入力されているか調べる
 		if (commonValidator.notSetOn(capacity)) {
 			_request.setAttribute("Emessage", EM30);
 			return false;
 		}
-		//定員が数字になっているか調べる
+		// 定員が数字になっているか調べる
 		if (commonValidator.notNumericOn(capacity)) {
 			_request.setAttribute("Emessage", EM31);
 			return false;
 		}
-		//数字に変換した定員を取得する
+		// 数字に変換した定員を取得する
 		int capacityNumber = commonValidator.getNumber();
-		//事業所情報があるか調べる
+		// 事業所情報があるか調べる
 		if (commonValidator.notSetOn(officeName)) {
 			_request.setAttribute("Emessage", EM33);
 			return false;
 		}
-		//利用停止開始日時が設定されているか調べる
+		// 利用停止開始日時が設定されているか調べる
 		Timestamp stopStartDate = null;
 		if (!commonValidator.notSetOn(stopStartDay)) {
-			//日付のフォーマットが正しいか調べる
+			// 日付のフォーマットが正しいか調べる
 			if (commonValidator.notDateOn(stopStartDay, stopStartHour, stopStartMinute)) {
 				_request.setAttribute("Emessage", EM38);
 				return false;
 			}
 			stopStartDate = commonValidator.getDate();
 		}
-		//利用停止終了日時が設定されているか調べる
+		// 利用停止終了日時が設定されているか調べる
 		Timestamp stopEndDate = null;
 		if (!commonValidator.notSetOn(stopEndDay)) {
-			//日付のフォーマットが正しいか調べる
+			// 日付のフォーマットが正しいか調べる
 			if (commonValidator.notDateOn(stopEndDay, stopEndHour, stopEndMinute)) {
 				_request.setAttribute("Emessage", EM38);
 				return false;
 			}
 			stopEndDate = commonValidator.getDate();
 		}
-		//全てのチェックが通るなら、リソースのDTOを作成する
+		// 全てのチェックが通るなら、リソースのDTOを作成する
 		_resource = new Resource(resourceId, resourceName, officeName, category, capacityNumber, supplement, 0,
 				facility, stopStartDate, stopEndDate);
 		return true;
 	}
 
-	/**リソースの登録を行う
+	/**
+	 * リソースの登録を行う
+	 *
 	 * @return 遷移先のアドレス
 	 */
 	private String regist() {
 		RegistResourceService registResourceService = new RegistResourceService(_resource);
 
-		//バリデーションチェック
+		// バリデーションチェック
 		if (registResourceService.validate()) {
 			try {
 				registResourceService.execute();
-				//登録できた件数を取得する
+				// 登録できた件数を取得する
 				int result = registResourceService.getResult();
-				//自動生成したIDを取得する
+				// 自動生成したIDを取得する
 				String resourceId = registResourceService.getResourceId();
-				//正しく1件登録できたか、IDが設定されているか調べる
-				if(resourceId==null){
+				// 正しく1件登録できたか、IDが設定されているか調べる
+				if (resourceId == null) {
 					_log.error("resourceId overflow");
 					return ERROR_PAGE;
-				}else if(result!=1){
+				} else if (result != 1) {
 					_log.error("regist failed");
 					return ERROR_PAGE;
-				}else{
-					Resource resultResource=registResourceService.getResultResource();
-					if( (resultResource.getUsageStopStartDate() != null) && (resultResource.getUsageStopEndDate() != null) ){
-			            //利用停止期間をフォーマットに即して変換してセット
-			            String format = "yyyy/MM/dd　H時m分";
-			            String stopStartDate = new SimpleDateFormat(format).format(resultResource.getUsageStopStartDate());
-			            String stopEndDate = new SimpleDateFormat(format).format(resultResource.getUsageStopEndDate());
-			                _request.setAttribute("stopStartDate", stopStartDate);
-			                _request.setAttribute("stopEndDate", stopEndDate);
-			            }
+				} else {
+					Resource resultResource = registResourceService.getResultResource();
+					if ((resultResource.getUsageStopStartDate() != null)
+							&& (resultResource.getUsageStopEndDate() != null)) {
+						// 利用停止期間をフォーマットに即して変換してセット
+						String format = "yyyy/MM/dd　H時m分";
+						String stopStartDate = new SimpleDateFormat(format)
+								.format(resultResource.getUsageStopStartDate());
+						String stopEndDate = new SimpleDateFormat(format).format(resultResource.getUsageStopEndDate());
+						_request.setAttribute("stopStartDate", stopStartDate);
+						_request.setAttribute("stopEndDate", stopEndDate);
+					}
 					_request.setAttribute("resource", resultResource);
 					_request.setAttribute("Pmessage", PM06);
 					return RESOURCE_DETAILS;
@@ -199,7 +207,7 @@ public class SetResourceDetailsHandler implements Handler {
 				return ERROR_PAGE;
 			}
 		} else {
-			//入力に不備があればリソース入力ページに戻す
+			// 入力に不備があればリソース入力ページに戻す
 			String validationMessage = registResourceService.getValidationMessage();
 			_request.setAttribute("Emessage", validationMessage);
 			return RESOURCE_REGIST_SERVLET;
@@ -207,29 +215,33 @@ public class SetResourceDetailsHandler implements Handler {
 
 	}
 
-	/**リソース情報を変更する
+	/**
+	 * リソース情報を変更する
+	 *
 	 * @return 遷移先のアドレス
 	 */
 	private String change() {
 		ChangeResourceService changeResourceService = new ChangeResourceService(_resource);
 
-		//バリデーションチェック
+		// バリデーションチェック
 		if (changeResourceService.validate()) {
 			try {
 				changeResourceService.execute();
-				//変更した件数を取得する
+				// 変更した件数を取得する
 				int result = changeResourceService.getResult();
-				//正しく1件登録できているか調べる
+				// 正しく1件登録できているか調べる
 				if (result == 1) {
-					Resource resultResource=changeResourceService.getResultResource();
-					if( (resultResource.getUsageStopStartDate() != null) && (resultResource.getUsageStopEndDate() != null) ){
-			            //利用停止期間をフォーマットに即して変換してセット
-			            String format = "yyyy/MM/dd　H時m分";
-			            String stopStartDate = new SimpleDateFormat(format).format(resultResource.getUsageStopStartDate());
-			            String stopEndDate = new SimpleDateFormat(format).format(resultResource.getUsageStopEndDate());
-			                _request.setAttribute("stopStartDate", stopStartDate);
-			                _request.setAttribute("stopEndDate", stopEndDate);
-			            }
+					Resource resultResource = changeResourceService.getResultResource();
+					if ((resultResource.getUsageStopStartDate() != null)
+							&& (resultResource.getUsageStopEndDate() != null)) {
+						// 利用停止期間をフォーマットに即して変換してセット
+						String format = "yyyy/MM/dd　H時m分";
+						String stopStartDate = new SimpleDateFormat(format)
+								.format(resultResource.getUsageStopStartDate());
+						String stopEndDate = new SimpleDateFormat(format).format(resultResource.getUsageStopEndDate());
+						_request.setAttribute("stopStartDate", stopStartDate);
+						_request.setAttribute("stopEndDate", stopEndDate);
+					}
 					_request.setAttribute("resource", resultResource);
 					_request.setAttribute("Pmessage", PM06);
 					return RESOURCE_DETAILS;
@@ -242,7 +254,7 @@ public class SetResourceDetailsHandler implements Handler {
 				return ERROR_PAGE;
 			}
 		} else {
-			//入力に不備があればリソース入力ページに戻す
+			// 入力に不備があればリソース入力ページに戻す
 			String validationMessage = changeResourceService.getValidationMessage();
 			_request.setAttribute("Emessage", validationMessage);
 			return RESOURCE_REGIST_SERVLET;
