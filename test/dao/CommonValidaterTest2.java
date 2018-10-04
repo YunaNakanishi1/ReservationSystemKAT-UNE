df6d13db1e1627c25d66db4fe81dde71a4f98271
z00s600109 <z00s600109@ZRFG046915.rfg.rfgad.ricoh.com>
2018-10-04 15:52:48 +0900

    CommonValidatorTest1,2,3

---
+++ test/dao/CommonValidaterTest2.java
@@ -0,0 +1,28 @@
+package dao;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.junit.Test;
+
+import handler.CommonValidator;
+
+public class CommonValidaterTest2 {
+
+
+	@Test
+	public void testNotDateOn1() throws Exception, IllegalArgumentException, InvocationTargetException {
+		CommonValidator commonValidator=new CommonValidator();
+		java.lang.reflect.Method method =CommonValidator.class.getDeclaredMethod("notDateOn", String.class,String.class,String.class);
+
+		method.setAccessible(true);
+		boolean actual=(boolean)method.invoke(commonValidator,"02/28","00","00");
+
+		assertThat(actual,is(false));
+	}
+	
+	
+
+}
