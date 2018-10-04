3e1dc5ba803f45f9dd3963cccf73f698fce550a6
z00h230741 <z00h230741@A050931913.rfg.rfgad.ricoh.com>
2018-10-04 13:01:49 +0900

    officedao単体テスト

---
+++ test/dao/OfficeDaoTest_dataSet1.java
@@ -0,0 +1,35 @@
+/**
+ *
+ */
+package dao;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * @author z00s600114
+ *
+ */
+public class OfficeDaoTest_dataSet1 {
+
+	/**
+	 * {@link dao.OfficeDao#officeName()} のためのテスト・メソッド。
+	 */
+	@Test
+	public void testOfficeName() throws SQLException  {
+//		fail("まだ実装されていません");
+		OfficeDao od = new OfficeDao();
+		List<String> officeList = new ArrayList<String>();
+		officeList.add("晴海");
+
+		for(int i=0;i<officeList.size();i++){
+			assertThat(od.officeName().get(i),is(officeList.get(i)));
+		}
+	}
+}
---
+++ test/dao/SampleTest.java
@@ -0,0 +1,14 @@
+package dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void testOfficeName() {
+		fail("まだ実装されていません");
+	}
+
+}
