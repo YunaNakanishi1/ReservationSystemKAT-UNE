20365ae570c7902539473bdaf7cedbeeae66b6d6
z00s600114 <z00s600114@ZRFG046920.rfg.rfgad.ricoh.com>
2018-10-04 11:26:12 +0900

    コメント追加中

--- src/service/RegistResourceService.java
+++ src/service/RegistResourceService.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright© Ricoh IT Solutions Co.,Ltd.
+ * All Rights Reserved.
+ */
 package service;
 
 import java.sql.SQLException;
@@ -12,35 +16,71 @@ import dto.Resource;
 public class RegistResourceService implements Service {
 
 	private String _validationMessage;
+	//"ユーザIDは必須入力です"等、画面上に出すメッセージ
+
 	private Resource _inputResource;
-	private int _result;
-	private String _resourceId;
+	/*SetResourceDetailsHandlerでRegistResourceServiceコンストラクタ呼び
+	 出し時にもらうResourceのDTOを格納*/
+
+	private int _result; //executeUpdate()の結果、返ってくる数値を格納
+	private String _resourceId; //リソースID
+
 	private Resource _resultResource;
+	//resourceDaoのdisplayDetails(String)メソッド実行で返ってくるResourceのDTOを格納
+
 	private static final int NUMBER_LENGTH=9;
+	//リソースIDは「r+9桁の数字」で構成
+
+
 
 	public RegistResourceService(Resource resource) {
 		super();
 		_inputResource = resource;
 	}
 
-	/* 機能に依存するバリデーションチェックを行う.
+
+
+	/* リソース登録時のバリデーションチェックを行う.
 	 * @see service.Service#validate()
 	 */
 	@Override
 	public boolean validate() {
 		ServiceValidator serviceValidator = new ServiceValidator();
+
+		//入力した内容がチェックに引っかかった場合にはfalseが返ってくる
 		boolean validate = serviceValidator.setResourseDetailValidate(_inputResource);
+
 		if (!validate) {
+			/*チェックに引っかかった場合はメッセージを画面に表示する。
+			Handler側でメッセージのsetAttributeを行うため
+			ここではフィールドに格納しておく。
+			 ※フィールドはgetメソッドで取得可能  */
 			_validationMessage = serviceValidator.getValidationMessage();
 		}
 		return validate;
 	}
 
-	/* リソース情報を変更する.
+
+
+
+	/* リソース情報を登録する.
 	 * @see service.Service#execute()
 	 */
 	@Override
 	public void execute() throws SQLException {
+		/*
+		【リソースID】「r」始まりで、加えて9桁の数字を組み合わせた10桁の文字列
+
+		●リソース登録時には登録リソースにIDをシステム内で自動的に振り当てる。
+
+		≪リソース登録時内部処理≫
+		既リソースIDの数字部分の最大値に1を加えたものをリソースIDとして振り当てる
+		⇒既登録リソースIDの最大値が「999999999」(9桁)の場合、新たに登録する
+		リソースIDは「最大値＋１」になるので、「10000000000」(10桁)になってしまう
+
+		*/
+
+
 		// 全リソースIDを取得
 		ResourceDao resourceDao = new ResourceDao();
 		String maxId = resourceDao.getMaxId();
@@ -50,20 +90,21 @@ public class RegistResourceService implements Service {
 		//idがnullではないとき
 		//rを取り除く、int型に変換する
 		if(maxId!=null){
-		String maxIdNumber = maxId.replace("r", "");
-		maxIdInt = Integer.parseInt(maxIdNumber);
+			String maxIdNumber = maxId.replace("r", "");
+			maxIdInt = Integer.parseInt(maxIdNumber);
 		}
 		maxIdInt++;
 
 		String idNumber=Integer.toString(maxIdInt);
 
-		// 最大桁に対して桁数が残っているか調べる
-		int remainingLength = NUMBER_LENGTH - idNumber.length();
-		if (remainingLength <= 0) {
+
+		if (NUMBER_LENGTH < idNumber.length()) {
 			_resourceId = null;
 			return;
 		}
 		// IDの補完する部分を求める
+		// 最大桁に対して桁数が残っているか調べる
+		int remainingLength = NUMBER_LENGTH - idNumber.length();
 		String formerOfResourceId = "";
 		for (int i = 0; i < remainingLength; i++) {
 			formerOfResourceId = "0" + formerOfResourceId;
@@ -81,6 +122,11 @@ public class RegistResourceService implements Service {
 		_resultResource=resourceDao.displayDetails(_resourceId);
 
 	}
+
+
+
+
+
 
 	public String getValidationMessage() {
 		return _validationMessage;
