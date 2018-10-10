6754bfb6802bf35625ea28e4565071bc7c87f32e
p000527259 <p000527259@A062844480.rfg.rfgad.ricoh.com>
2018-10-10 09:53:36 +0900

    句読点

--- src/handler/MessageHolder.java
+++ src/handler/MessageHolder.java
@@ -9,30 +9,30 @@ package handler;
 *@author リコーITソリューションズ株式会社 KAT-UNE
 */
 public class MessageHolder {
-	public static final String PM01 = "ログアウトしました";
-	public static final String PM06 = "リソースの登録が完了しました";
-	public static final String PM08 = "リソースの削除が完了しました";
-	public static final String EM01 = "ユーザIDは必須入力です";
-	public static final String EM02 = "ユーザIDは半角英数字で入力してください";
-	public static final String EM03 = "ユーザIDは8桁で入力してください";
-	public static final String EM04 = "パスワードは必須入力です";
-	public static final String EM05 = "パスワードは半角英数字で入力してください";
-	public static final String EM06 = "認証に失敗しました";
-	public static final String EM07 = "データベースへの接続に失敗しました";
-	public static final String EM08 = "検索結果は0件です";
-	public static final String EM22 = "補足は500文字以内で入力してください";
-	public static final String EM27 = "リソース名は必須入力です";
-	public static final String EM28 = "リソース名は30文字以内で入力してください";
-	public static final String EM30 = "定員は必須入力です";
-	public static final String EM31 = "定員は半角の整数で入力してください";
-	public static final String EM32 = "定員は1～999または0で入力してください";
+	public static final String PM01 = "ログアウトしました。";
+	public static final String PM06 = "リソースの登録が完了しました。";
+	public static final String PM08 = "リソースの削除が完了しました。";
+	public static final String EM01 = "ユーザIDは必須入力です。";
+	public static final String EM02 = "ユーザIDは半角英数字で入力してください。";
+	public static final String EM03 = "ユーザIDは8桁で入力してください。";
+	public static final String EM04 = "パスワードは必須入力です。";
+	public static final String EM05 = "パスワードは半角英数字で入力してください。";
+	public static final String EM06 = "認証に失敗しました。";
+	public static final String EM07 = "データベースへの接続に失敗しました。";
+	public static final String EM08 = "検索結果は0件です。";
+	public static final String EM22 = "補足は500文字以内で入力してください。";
+	public static final String EM27 = "リソース名は必須入力です。";
+	public static final String EM28 = "リソース名は30文字以内で入力してください。";
+	public static final String EM30 = "定員は必須入力です。";
+	public static final String EM31 = "定員は半角の整数で入力してください。";
+	public static final String EM32 = "定員は1～999または0で入力してください。";
 	public static final String EM33 = "事業所は必須入力です";
-	public static final String EM34 = "利用停止開始日を入力している場合、 利用停止終了日を入力してください";
-	public static final String EM35 = "利用停止終了日を入力している場合、 利用停止開始日を入力してください";
-	public static final String EM36 = "利用停止終了日時は利用停止開始日時より後の日時を入力してください";
+	public static final String EM34 = "利用停止開始日を入力している場合、 利用停止終了日を入力してください。";
+	public static final String EM35 = "利用停止終了日を入力している場合、 利用停止開始日を入力してください。";
+	public static final String EM36 = "利用停止終了日時は利用停止開始日時より後の日時を入力してください。";
 	public static final String EM37 = "カテゴリ名を選択してください";
 	public static final String EM38 = "日付が正しく入力されていません";
-	public static final String EM39 = "ログインがタイムアウト（時間切れ）となりました。再度ログインしてください";
+	public static final String EM39 = "ログインがタイムアウト（時間切れ）となりました。再度ログインしてください。";
 
 	private MessageHolder(){
 
