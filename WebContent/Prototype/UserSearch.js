e0faef2e5c42ebe7df650ac3712f5a8281144b28
z00s600124 <z00s600124@ZRFG046930.rfg.rfgad.ricoh.com>
2018-10-17 14:17:48 +0900

    slider

--- WebContent/Prototype/UserSearch.js
+++ WebContent/Prototype/UserSearch.js
@@ -15,21 +15,12 @@ function addSearch(id)
     <input type="text" name=""> <input class="button" type = "submit" value = "検索">\
     <br>\
     <select name="userList" size="5" style="width:200px">\
-    <option value="u0000001">理光太郎</option>\
-    <option value="u0000001">理光太郎</option>\
-    <option value="u0000001">理光太郎</option>\
-    <option value="u0000001">理光太郎</option>\
-    <option value="u0000001">理光太郎</option>\
-    <option value="u0000001">理光太郎</option>\
-    <option value="u0000001">理光太郎</option>\
-    <option value="u0000001">理光太郎</option>\
-    <option value="u0000001">理光太郎</option>\
-    <option value="u0000001">理光太郎</option>\
-    <option value="u0000001">理光太郎</option>\
-    <option value="u0000001">理光太郎</option>\
-    <option value="u0000001">理光太郎</option>\
-    <option value="u0000001">理光太郎</option>\
-    <option value="u0000001">理光太郎</option>\
+    <option value="u0123456">理光太郎 (u0123456)</option>\
+    <option value="u1111222">理光二郎 (u1111222)</option>\
+    <option value="u0000003">理光三郎 (u0000003)</option>\
+    <option value="u0000004">理光四郎 (u0000004)</option>\
+    <option value="u0000005">理光五郎 (u0000005)</option>\
+    <option value="u0000006">理光六郎 (u0000006)</option>\
     </select>\
     <br>\
     <input class="button" type = "submit" onclick="closeSearch(\''+id+'\');" value = "選択"> <input class="button" type = "submit" onclick="closeSearch(\''+id+'\');" value = "閉じる">\
