551c8481920c1360c915fbee13d13fa41c157fd6
z00s600114 <z00s600114@ZRFG046920.rfg.rfgad.ricoh.com>
2018-11-12 13:32:11 +0900

    aa

--- src/dao/ReservationDao.java
+++ src/dao/ReservationDao.java
@@ -4,10 +4,44 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import dto.ReservationDto;
+
 /**
   * (6 8 10 11 12 13 17 18).
   * @author リコーITソリューションズ株式会社 KAT-UNE
   */
 public class ReservationDao {
+
+	private Connection _con = null;
+	private static Logger _log = LogManager.getLogger();
+
+	
+	
+	
+	/**
+	  * 
+	  * @param reserveId 予約ID
+	  * @return
+	  * @throws SQLException
+	  */
+	public ReservationDto queryById(int reserveId) throws SQLException {
+
+		DBHelper dbHelper = new DBHelper();
+		_con = dbHelper.connectDb(); //dbに接続
+
+		if (_con == null) {
+			_log.error("DatabaseConnectError");
+			throw new SQLException();	//エラー処理はハンドラーに任せる
+		}
+
+		return null;
+
+	}
 
 }
