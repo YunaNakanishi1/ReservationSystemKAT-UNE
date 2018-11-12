48ffc7e7f01a0ab1ff4823b5dad642e6b730f215
p000527259 <p000527259@A062844480.rfg.rfgad.ricoh.com>
2018-11-12 10:24:53 +0900

    今すぐ予約

---
+++ src/handler/PushQuickReservationButtonHandler.java
@@ -0,0 +1,29 @@
+/**
+ *
+ */
+package handler;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author p000527259
+ *
+ */
+public class PushQuickReservationButtonHandler implements Handler {
+	private static int THIRTY_MINUTES = 30;
+	private static int SIXTY_MINUTES = 60;
+	private static int ONE_HOUR = 1;
+	private static int ZERO = 0;
+	private static int TWENTY_FOUR = 24;
+
+
+	/* (非 Javadoc)
+	 * @see handler.Handler#handleService(javax.servlet.http.HttpServletRequest)
+	 */
+	@Override
+	public String handleService(HttpServletRequest request) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
