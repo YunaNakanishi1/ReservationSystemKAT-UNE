471b1ad7c18fb8b18fa6318ac687710fb138670c
z00h230741 <z00h230741@A050931913.rfg.rfgad.ricoh.com>
2018-11-12 11:38:22 +0900

    handler7

---
+++ src/handler/PushNewReservationButtonHandler.java
@@ -0,0 +1,57 @@
+package handler;
+
+import static handler.ViewHolder.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import dto.TimeDto;
+
+public class PushNewReservationButtonHandler implements Handler {
+	private static int THIRTY_MINUTES = 30;
+	private static int SIXTY_MINUTES = 60;
+	private static int ONE_HOUR = 1;
+	private static int ZERO = 0;
+	private static int TWENTY_FOUR = 24;
+	private Logger _log = LogManager.getLogger();
+
+	/* (非 Javadoc)
+	 * @see handler.Handler#handleService(javax.servlet.http.HttpServletRequest)
+	 */
+	@Override
+	public String handleService(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+	      //セッションは存在する
+		//HandleHelper.initializeAttributeForReservationRegist(session);
+		//実装未だ
+
+		//当日の日付取得, セット
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+		LocalDateTime usageDate = LocalDateTime.now();
+		String usageDateForReservationRegist = usageDate.format(formatter);
+		session.setAttribute("usageDateForReservationRegist", usageDateForReservationRegist);
+
+		Date currentTime = new Date();
+		TimeDto currentTimeDto = null;
+		try {
+			currentTimeDto = new TimeDto(currentTime);
+			} catch(MyException e) {
+				return ERROR_PAGE;
+			}
+
+	int hour = currentTimeDto.getHour();
+	int minutes = currentTimeDto.getMinutes();
+	return null;
+	}
+
+	// 初期値をsetAttributeする
+			String resourceId = _request.getParameter("resourceId");
+			_request.setAttribute("resourceId", resourceId);
+}
