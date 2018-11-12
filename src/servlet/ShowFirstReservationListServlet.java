a3fe8e8df2c48429a9023b30cd0a7e357cf4c75d
z00s600120 <z00s600120@ZRFG046926.rfg.rfgad.ricoh.com>
2018-11-12 11:01:04 +0900

    login修正

--- src/servlet/ShowFirstReservationListServlet.java
+++ src/servlet/ShowFirstReservationListServlet.java
@@ -2,11 +2,15 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import handler.Handler;
+import handler.ShowFirstReservationListHandler;
 
 /**
  * Servlet implementation class ShowFirstReservationListServlet
@@ -19,6 +23,12 @@ public class ShowFirstReservationListServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		Handler showFirstReservationListHandler = new ShowFirstReservationListHandler();
+		//遷移先URL
+        String view = showFirstReservationListHandler.handleService(request);
+
+        RequestDispatcher rd = request.getRequestDispatcher(view);
+        rd.forward(request, response);
 	}
 
 }
