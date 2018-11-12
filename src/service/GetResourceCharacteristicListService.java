a7f2b6be26dddb7a0c81d687342b2dc44949d9ec
z00h230741 <z00h230741@A050931913.rfg.rfgad.ricoh.com>
2018-11-12 17:29:38 +0900

    q

---
+++ src/service/GetResourceCharacteristicListService.java
@@ -0,0 +1,33 @@
+package service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import dao.FacilityDao;
+import dto.FacilityDto;
+
+/**
+ *
+ * @author z00h230741
+ *
+ */
+public class GetResourceCharacteristicListService implements Service{
+	private List<FacilityDto> _facilityList;
+
+	@Override
+	public boolean validate() {
+		return true;
+	}
+
+	@Override
+	public void execute() throws SQLException {
+		FacilityDao facilityDao = new FacilityDao();
+		_facilityList = facilityDao.queryAll();
+	}
+
+	public List<FacilityDto> getFacilityList() {
+		return _facilityList;
+	}
+
+
+}
