55fb393e84529c572e87e2ee1e669b2fb98c554c
z00s600120 <z00s600120@ZRFG046926.rfg.rfgad.ricoh.com>
2018-11-12 15:22:05 +0900

    予約一覧ページ途中

--- src/handler/ViewHolder.java
+++ src/handler/ViewHolder.java
@@ -9,7 +9,7 @@ public class ViewHolder {
 
 
     public static final String LOG_IN  = "/Login.jsp";
-    public static final String RESERVE_LIST  = "/WEB-INF/reserve/ReserveList.jsp";
+    public static final String RESERVE_LIST  = "/WEB-INF/reserve/ReservationList.jsp";
     public static final String RESOURCE_LIST  = "/WEB-INF/resource/ResourceList.jsp";
     public static final String RESOURCE_DETAILS  = "/WEB-INF/resource/ResourceDetails.jsp";
     public static final String RESOURCE_REGIST  = "/WEB-INF/resource/ResourceRegist.jsp";
