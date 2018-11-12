04c59b15893cc9a2ae8866b18fae22369db34deb
z00s600120 <z00s600120@ZRFG046926.rfg.rfgad.ricoh.com>
2018-11-12 13:21:30 +0900

    カテゴリ事業所サービス作成

--- src/service/GetOfficeAndCategoryService.java
+++ src/service/GetOfficeAndCategoryService.java
@@ -1,6 +1,12 @@
 package service;
 
 import java.sql.SQLException;
+import java.util.List;
+
+import dao.CategoryDao;
+import dao.OfficeDao;
+import dto.CategoryDto;
+import dto.OfficeDto;
 
 /**
  * 事業所とカテゴリの一覧を取得する.
@@ -9,17 +15,35 @@ import java.sql.SQLException;
  *
  */
 public class GetOfficeAndCategoryService implements Service {
+	private List<OfficeDto> _officeList;
+	private List<CategoryDto> _categoryList;
+
 
 	@Override
 	public boolean validate() {
-
-		return false;
+		return true;
 	}
 
 	@Override
 	public void execute() throws SQLException {
+		OfficeDao officeDao = new OfficeDao();
+		_officeList=officeDao.queryAll();
+
+		CategoryDao categoryDao = new CategoryDao();
+		_categoryList = categoryDao.queryAll();
+
 
 
 	}
+
+	public List<OfficeDto> getOfficeList() {
+		return _officeList;
+	}
+
+	public List<CategoryDto> getCategoryList() {
+		return _categoryList;
+	}
+
+
 
 }
