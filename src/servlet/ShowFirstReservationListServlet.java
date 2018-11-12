3967ae4b6d7c235d9bd4669befb50fa93c2db598
z00s600120 <z00s600120@ZRFG046926.rfg.rfgad.ricoh.com>
2018-11-12 10:54:07 +0900

    showfirstreservation途中

---
+++ src/servlet/ShowFirstReservationListServlet.java
@@ -0,0 +1,24 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ShowFirstReservationListServlet
+ */
+@WebServlet("/showfirstreservationlist")
+public class ShowFirstReservationListServlet extends HttpServlet {
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+}
