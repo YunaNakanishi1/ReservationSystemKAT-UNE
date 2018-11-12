a08d7b21b025e56c676a0d7f930f82124bd72f57
z00s600120 <z00s600120@ZRFG046926.rfg.rfgad.ricoh.com>
2018-11-12 11:39:19 +0900

    showfirstreservationlist途中

--- src/handler/ShowFirstReservationListHandler.java
+++ src/handler/ShowFirstReservationListHandler.java
@@ -1,10 +1,16 @@
 package handler;
 
+import static handler.ViewHolder.*;
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
+import dto.ReservationDto;
+import dto.TimeDto;
 
 public class ShowFirstReservationListHandler implements Handler{
 
@@ -14,7 +20,12 @@ public class ShowFirstReservationListHandler implements Handler{
 
 		String usageDateForReservationList = new SimpleDateFormat("yyyy/MM/dd").format(Calendar.getInstance());
 
-		return null;
+		session.setAttribute("reservationListForReservationList", new ArrayList<ReservationDto>());
+		session.setAttribute("usageDateForReservationList", usageDateForReservationList);
+		session.setAttribute("usageStartHourForReservationList", new TimeDto(0, 0));
+		session.setAttribute("usageEndHourForReservationList", new TimeDto(24,0));
+		session.setAttribute("displayOnlyMyReservation", true);
+		return SEARCH_RESERVATION_LIST_SERVLET;
 	}
 
 }
