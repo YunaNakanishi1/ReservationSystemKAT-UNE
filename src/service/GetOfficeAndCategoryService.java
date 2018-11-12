53232b57da13a13e649649d421d17e7281783b8d
z00s600120 <z00s600120@ZRFG046926.rfg.rfgad.ricoh.com>
2018-11-12 13:13:21 +0900

    カテゴリ事業所取得サービス

---
+++ src/service/GetOfficeAndCategoryService.java
@@ -0,0 +1,19 @@
+package service;
+
+import java.sql.SQLException;
+
+public class GetOfficeAndCategoryService implements Service {
+
+	@Override
+	public boolean validate() {
+
+		return false;
+	}
+
+	@Override
+	public void execute() throws SQLException {
+
+
+	}
+
+}
