af0c71d4c019071449dc11e97baae6da1060ac20
z00s600109 <z00s600109@ZRFG046915.rfg.rfgad.ricoh.com>
2018-11-13 16:46:45 +0900

    更新

--- src/servlet/DeleteReservationServlet.java
+++ src/servlet/DeleteReservationServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import handler.DeleteReservationHandler;
 import handler.Handler;
 
 /**
