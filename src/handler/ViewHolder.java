666a9122cbe12840623f7b16a884af3a15adfb84
z00h230741 <z00h230741@A050931913.rfg.rfgad.ricoh.com>
2018-11-13 14:40:52 +0900

    handler更新

--- src/handler/ViewHolder.java
+++ src/handler/ViewHolder.java
@@ -23,4 +23,6 @@ public class ViewHolder {
     public static final String SHOW_RESOURCE_SELECT_SERVLET="/reservesystem/showResourceSelect";
     public static final String QUIICK_RESERVATION = "/WEB-INF/reserve/QuickReservation.jsp";
     public static final String RESOURCE_SELECT = "/WEB-INF/resource/ResourceSelect.jsp";
+    public static final String DELETE_RESERVATION_SERVLET = "/reservesystem/deleteReservation";
+
 }
