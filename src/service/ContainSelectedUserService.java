656d25392425bd9d0600bc921aef38b8bd698738
z00s600114 <z00s600114@ZRFG046920.rfg.rfgad.ricoh.com>
2018-11-15 15:57:30 +0900

    a

--- src/service/ContainSelectedUserService.java
+++ src/service/ContainSelectedUserService.java
@@ -7,26 +7,41 @@ package service;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import dto.User;
+import exception.MyException;
 
 /**
- * (26).
+ * (8).
  * @author リコーITソリューションズ株式会社 KAT-UNE
  */
 public class ContainSelectedUserService implements Service{
 	private String _userId;
 	private List<User> _userList;
+	private static Logger _log = LogManager.getLogger();
 
 
-	public ContainSelectedUserService(String userId,List<User> uesrList){
 
+	public ContainSelectedUserService(String userId,List<User> userList){
+		if(_userList == null){
+			_log.error("_userList == null");
+            throw new MyException();   //エラー処理はハンドラーに任せる
+		}
+
+		_userId = userId;
+		_userList = userList;
 	}
 
 
 	@Override
 	public boolean validate() {
-		// TODO 自動生成されたメソッド・スタブ
-		return false;
+		if(_userId == null){
+			return true;
+
+		}
+		return true;
 	}
 
 
