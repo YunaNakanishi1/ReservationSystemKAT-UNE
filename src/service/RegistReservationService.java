cee69bf1dc5b04bd0ab979ae533eba3c9c954fe7
z00s600120 <z00s600120@ZRFG046926.rfg.rfgad.ricoh.com>
2018-11-15 13:49:39 +0900

    予約登録サービス宣言

---
+++ src/service/RegistReservationService.java
@@ -0,0 +1,19 @@
+package service;
+
+import java.sql.SQLException;
+
+public class RegistReservationService implements Service {
+
+	@Override
+	public boolean validate() {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+	@Override
+	public void execute() throws SQLException {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
