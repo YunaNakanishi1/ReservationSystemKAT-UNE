54d8e9c11bdf321db5033f52fb2fa84f3fbcb211
p000527259 <p000527259@A062844480.rfg.rfgad.ricoh.com>
2018-11-15 13:47:29 +0900

    変更

--- src/handler/ViewHolder.java
+++ src/handler/ViewHolder.java
@@ -28,6 +28,7 @@ public class ViewHolder {
     public static final String SEARCH_RESOURCE_LIST_SERVLET = "/reservesystem/searchResourceList";
     public static final String SHOW_RESERVATION_DETAILS_SERVLET = "/reservesystem/showReservationDetails";
     public static final String LOGIN_FIRST_RESERVATION_LIST_SERVLET="/reservesystem/showfirstreservationlist";
-
+    public static final String SHOW_RESERVATION_CHANGE_SERVLET = "showreservechange";
+    public static final String PUSH_CHANGE_BUTTON_ON_RESERVATION_DETAILS_SERVLET = "pushChangeButtonOnReservationDetails";
 
 }
