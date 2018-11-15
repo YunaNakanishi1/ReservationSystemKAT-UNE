72119fdca882540fe28a36ba10e0dbb7fa27d231
z00s600120 <z00s600120@ZRFG046926.rfg.rfgad.ricoh.com>
2018-11-15 11:13:14 +0900

    viewholder変更

--- src/handler/ViewHolder.java
+++ src/handler/ViewHolder.java
@@ -28,6 +28,7 @@ public class ViewHolder {
     public static final String SEARCH_RESOURCE_LIST_SERVLET = "/reservesystem/searchResourceList";
     public static final String SHOW_RESERVATION_DETAILS_SERVLET = "/reservesystem/showReservationDetails";
     public static final String LOGIN_FIRST_RESERVATION_LIST_SERVLET="/reservesystem/showfirstreservationlist";
+    public static final String SHOW_RESERVATION_CHANGE_SERVLET="showreservechange";
 
 
 }
