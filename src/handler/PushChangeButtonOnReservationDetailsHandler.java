bff99f0a078261c7a95dca2fc1a56edeee1b4b7b
z00s600114 <z00s600114@ZRFG046920.rfg.rfgad.ricoh.com>
2018-11-16 14:22:50 +0900

    aa

--- src/handler/PushChangeButtonOnReservationDetailsHandler.java
+++ src/handler/PushChangeButtonOnReservationDetailsHandler.java
@@ -116,7 +116,10 @@ public class PushChangeButtonOnReservationDetailsHandler implements Handler {
 				getReservationListBetweenDateService.execute();
 
 				List<ReservationDto> reservationList=getReservationListBetweenDateService.getReservationList();
-				//System.out.println(reservationList);
+
+				System.out.println(reservationList.get(0).getReservationName());
+
+				System.out.println(reservationList.get(1).getReservationName());
 				if(reservationList.size()==1){
 					if(reservationList.get(0).getReservationId()!=reservation.getReservationId()){
 						_log.error("anothr reservation exist id");
