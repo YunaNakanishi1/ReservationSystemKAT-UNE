622d6c44e79c94bc6482e5334c01a651da0849b9
z00s600120 <z00s600120@ZRFG046926.rfg.rfgad.ricoh.com>
2018-11-16 10:04:23 +0900

    別タブリソース作成

--- src/servlet/ShowResourceDetailsTabServlet.java
+++ src/servlet/ShowResourceDetailsTabServlet.java
@@ -6,11 +6,15 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import handler.Handler;
+import handler.ShowResourceDetailsTabHandler;
 
 /**
 *
@@ -26,8 +30,11 @@ public class ShowResourceDetailsTabServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		Handler handler = new ShowResourceDetailsTabHandler();
+		String view = handler.handleService(request);
+
+		RequestDispatcher rd = request.getRequestDispatcher(view);
+		rd.forward(request, response);
 	}
 
 }
