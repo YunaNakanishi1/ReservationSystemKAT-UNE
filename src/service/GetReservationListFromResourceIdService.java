1a32deccc7f38d6d80cc9492a2c61e9dc6f4c740
z00h230741 <z00h230741@A050931913.rfg.rfgad.ricoh.com>
2018-11-19 12:20:49 +0900

    a

--- src/service/GetReservationListFromResourceIdService.java
+++ src/service/GetReservationListFromResourceIdService.java
@@ -2,34 +2,41 @@ package service;
 
 import java.sql.SQLException;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import com.sun.corba.se.spi.orbutil.fsm.Guard.Result;
-
 import dao.ReservationDao;
 import dto.ReservationDto;
-import dto.Resource;
+import exception.MyException;
 
 /**
  * サーブレット番号：6
  * @author リコーITソリューションズ株式会社 KAT-UNE
  */
-public class GetReservationListFromResourceIdService {
+public class GetReservationListFromResourceIdService implements Service{
 
 	private ReservationDto resultList;
-	private Resource _resource;
 	private String _resourceId;
-	private static Logger _log = LogManager.getLogger();
+
+
+	public GetReservationListFromResourceIdService(String resourceId){
+		if(resourceId == null){
+			throw new MyException();
+		}
+		this._resourceId = resourceId;
+	}
+
+	@Override
+	public boolean validate() {
+		return true;
+	}
 
 	@Override
 	public void execute() throws SQLException {
 		ReservationDao reservationDao = new ReservationDao();
-		_resource = reservationDao.queryByResourceId(_resourceId);
-		reservationDao.queryByResourceId(_resourceId){
-			//queryByResourceId作成
-			return resultList;
-		}
+		reservationDao.queryByResourceId(_resourceId);
 	}
+
+	public Resou getList(){
+		return resultList;
+	}
+
 
 }
