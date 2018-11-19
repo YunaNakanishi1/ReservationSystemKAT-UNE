26706283d00c23bd929c2e3182e11177d9ac9a68
z00h230741 <z00h230741@A050931913.rfg.rfgad.ricoh.com>
2018-11-19 14:27:41 +0900

    更新

--- src/handler/PushDeleteButtonOnResourceDeleteConfirmHandler.java
+++ src/handler/PushDeleteButtonOnResourceDeleteConfirmHandler.java
@@ -3,10 +3,17 @@ package handler;
 import static handler.MessageHolder.*;
 import static handler.ViewHolder.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import dto.ReservationDto;
+import service.GetReservationListFromResourceIdService;
 
 
 public class PushDeleteButtonOnResourceDeleteConfirmHandler implements Handler{
@@ -15,6 +22,7 @@ public class PushDeleteButtonOnResourceDeleteConfirmHandler implements Handler{
 
 	@Override
 	public String handleService(HttpServletRequest request) {
+		HttpSession session =request.getSession(true);
 
 	//リクエストからresourceIdをString型で受け取る
 	String resourceId = request.getParameter("resourceId");
@@ -24,10 +32,30 @@ public class PushDeleteButtonOnResourceDeleteConfirmHandler implements Handler{
 
 	if(checkBox != null){
 
+
+
+
 	}else{
 		request.setAttribute("messageForResourceDeleteConfirm",EM43);
 		return RESOURCE_DELETE_CONFIRM;
 	}
+	//sessionからreservationDtoを取得
+	List<ReservationDto> reservationList = (List<ReservationDto>)session.getAttribute("reservationListForResourceDeleteConfirm");
+	int count = 1;
+
+	List<ReservationDto> resultList =new ArrayList<ReservationDto>();
+	GetReservationListFromResourceIdService getReservationListFromResourceIdService=new GetReservationListFromResourceIdService(resourceId);
+	resultList=getReservationListFromResourceIdService.getList();
+
+	//結果のリストの各要素について
+	for(ReservationDto result : resultList){
+		//sessionから取得したリストの各要素について
+		for(ReservationDto reservationDto : reservationList){
+			if() //ここから
+
+		}
+	}
+
 
 	}
 
