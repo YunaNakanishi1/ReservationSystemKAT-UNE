3c2fda0e39c8df7c51c3c7f1070b6c001f512dff
z00s600109 <z00s600109@ZRFG046915.rfg.rfgad.ricoh.com>
2018-11-19 16:02:20 +0900

    更新

--- src/servlet/PushDeleteButtonOnResourceDetailsServlet.java
+++ src/servlet/PushDeleteButtonOnResourceDetailsServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import handler.Handler;
+import handler.PushDeleteButtonOnResourceDetailsHandler;
 
 /**
  * サーブレット番号：6
