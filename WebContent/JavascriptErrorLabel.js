e704366d3980f2bb1b22a1aa106ea4978b11cfd8
z00s600124 <z00s600124@ZRFG046930.rfg.rfgad.ricoh.com>
2018-11-21 15:36:27 +0900

    zzz

--- WebContent/JavascriptErrorLabel.js
+++ WebContent/JavascriptErrorLabel.js
@@ -1,9 +1,17 @@
 //ページが読み込まれるたび、「Javascriptが有効ではない」と表示されるラベルを
 //Javascriptで消す。Javascriptが無効化されていたら消せないので表示される
-window.onload =function(){
-    var jsBox = document.getElementById("JavascriptLabelBox");
-    if(jsBox != null){
-    	jsBox.innerHTML = "";
-    }
+//window.onload =function(){
+//	var jsBox = document.getElementById("JavascriptLabelBox");
+//	if(jsBox != null){
+//		jsBox.innerHTML = "";
+//	}
+//
+//}
 
-}
+document.observe("dom:loaded",
+		function() {
+	var jsBox = document.getElementById("JavascriptLabelBox");
+	if(jsBox != null){
+		jsBox.innerHTML = "";
+	}
+});
